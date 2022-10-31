6e890c50fe3ae91cd9cd579d8509aeff4a5c0fcc
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-10-31 14:36:15 +0000

    Update L1-5.cs

--- First Steps in Coding/Lekcia 1/L1-5.cs
+++ First Steps in Coding/Lekcia 1/L1-5.cs
@@ -1,15 +1,15 @@
-﻿//using System;
+using System;
 
-//namespace MyApp // Note: actual namespace depends on the project name.
-//{
-//    internal class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            string name = Console.ReadLine();
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
 
-//            Console.WriteLine("Hello, " + name + "!");  
+            Console.WriteLine("Hello, " + name + "!");
 
-//        }
-//    }
-//}
+        }
+    }
+}
