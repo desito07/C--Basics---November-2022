b1647a1e2d76b0f0ab761f9a5fba04c25790f20a
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-10-31 16:34:49 +0000

    Add files via upload

---
+++ First Steps in Coding/Yprajnenie 1/E1-4.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberPages = int.Parse(Console.ReadLine());
+            int pagesPerHour = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+
+            int TimeReadinHours = numberPages / pagesPerHour;
+            int daysNeeded = TimeReadinHours / days;
+
+            Console.WriteLine(daysNeeded);
+        }
+
+    }
+}
