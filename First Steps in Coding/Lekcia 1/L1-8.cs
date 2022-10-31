10b1b3d1cd62e49d77ccd633ede9cc4fe38c8d48
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-10-31 14:37:56 +0000

    Update L1-8.cs

--- First Steps in Coding/Lekcia 1/L1-8.cs
+++ First Steps in Coding/Lekcia 1/L1-8.cs
@@ -1,21 +1,20 @@
-﻿//using System;
+using System;
 
-//namespace MyApp // Note: actual namespace depends on the project name.
-//{
-//    internal class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            int numberPacksDogs = int.Parse(Console.ReadLine());
-//            int numberPacksCats = int.Parse(Console.ReadLine());
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberPacksDogs = int.Parse(Console.ReadLine());
+            int numberPacksCats = int.Parse(Console.ReadLine());
 
-//            double totalCostDogs = numberPacksDogs * 2.5;
-//            double totalCostCats = numberPacksCats * 4;
+            double totalCostDogs = numberPacksDogs * 2.5;
+            double totalCostCats = numberPacksCats * 4;
 
-//            Console.WriteLine($"{totalCostDogs + totalCostCats} lv.");
-//        }
+            Console.WriteLine($"{totalCostDogs + totalCostCats} lv.");
+        }
 
-//    }
-//}
-
+    }
+}
 
