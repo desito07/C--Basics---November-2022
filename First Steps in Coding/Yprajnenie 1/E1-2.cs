c80b97eee0751cd827635ec991e793c72f2bff3b
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-10-31 15:35:06 +0000

    Add files via upload

---
+++ First Steps in Coding/Yprajnenie 1/E1-2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radians = double.Parse(Console.ReadLine());
+
+            double degree = radians * 180 / Math.PI;
+
+            Console.WriteLine(degree);
+
+        }
+
+    }
+}
