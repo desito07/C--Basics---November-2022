c7cb998f88652ac01eb5f4ffc231d22c04f81771
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-10-31 14:35:20 +0000

    Update L1-4.cs

--- First Steps in Coding/Lekcia 1/L1-4.cs
+++ First Steps in Coding/Lekcia 1/L1-4.cs
@@ -1,16 +1,16 @@
-﻿//using System;
+using System;
 
-//namespace MyApp // Note: actual namespace depends on the project name.
-//{
-//    internal class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            double number = double.Parse(Console.ReadLine());
-           
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
 
-//            Console.WriteLine(number * 2.54);
 
-//        }
-//    }
-//}
+            Console.WriteLine(number * 2.54);
+
+        }
+    }
+}
