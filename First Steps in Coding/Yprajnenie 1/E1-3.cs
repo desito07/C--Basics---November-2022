7910255b6c5a4b3f9bdfda64097ddceffed72264
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-10-31 16:12:29 +0000

    Add files via upload

---
+++ First Steps in Coding/Yprajnenie 1/E1-3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double depositSum = double.Parse(Console.ReadLine());
+            int periodMonths = int.Parse(Console.ReadLine());
+            double interest = double.Parse(Console.ReadLine());
+
+            double interestTotalSum = depositSum * interest / 100;
+            double interestPerMonth = interestTotalSum / 12;
+
+            double sum = depositSum + periodMonths * interestPerMonth;
+
+            Console.WriteLine(sum);
+
+        }
+
+    }
+}
