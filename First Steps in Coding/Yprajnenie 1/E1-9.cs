931f6518e9e2ec25e29e1ebb055d2fe3136650e9
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-01 22:50:16 +0000

    Add files via upload

---
+++ First Steps in Coding/Yprajnenie 1/E1-9.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double percentage = double.Parse(Console.ReadLine());
+
+            double volume = length * width * height;
+            double volumeInLiters = volume / 1000;
+
+            double spaceNeeded = volumeInLiters - (volumeInLiters * percentage / 100);
+
+            Console.WriteLine(spaceNeeded);
+
+        }
+
+    }
+}
