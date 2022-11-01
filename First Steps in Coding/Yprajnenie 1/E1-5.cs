7550d6aa25189f161505c516262ecb0bc3d8f492
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-01 17:39:02 +0000

    Add files via upload

---
+++ First Steps in Coding/Yprajnenie 1/E1-5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pensPackageNumber = int.Parse(Console.ReadLine());
+            int highlitherPackageNumber = int.Parse(Console.ReadLine());
+            int cleaningstuffPackageNumber = int.Parse(Console.ReadLine());
+            int discount = int.Parse(Console.ReadLine());
+
+            double pensCost = pensPackageNumber * 5.80;
+            double lighlightersCost = highlitherPackageNumber * 7.20;
+            double cleaningstuffCost = cleaningstuffPackageNumber * 1.20;
+            double totalCost = pensCost + lighlightersCost + cleaningstuffCost;
+
+            double totalPrice = totalCost - (totalCost * discount / 100);
+
+            Console.WriteLine(totalPrice);
+        }
+
+    }
+}
