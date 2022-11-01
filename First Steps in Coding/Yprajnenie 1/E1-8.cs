8f528debff9c0825af7abf861fc2a25dcfa6a10d
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-01 22:29:33 +0000

    Add files via upload

---
+++ First Steps in Coding/Yprajnenie 1/E1-8.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int annualCostPerYear = int.Parse(Console.ReadLine());
+
+            double shoes = annualCostPerYear - (annualCostPerYear * 0.4);
+            double kit = shoes - (shoes * 0.2);
+            double ball = kit * 1/4 ;
+            double accesories = ball * 1/5;
+
+            double costs = shoes + kit + ball + accesories + annualCostPerYear;
+
+            Console.WriteLine(costs);
+        }
+
+    }
+}
