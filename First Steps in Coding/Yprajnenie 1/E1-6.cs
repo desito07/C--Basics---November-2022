39459bb28fc3b82e2fa0888bcdf1f4aec787d0a9
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-01 22:17:36 +0000

    Add files via upload

---
+++ First Steps in Coding/Yprajnenie 1/E1-6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nylonQuantityNeeded = int.Parse(Console.ReadLine());
+            double paintQuantityNeeded = double.Parse(Console.ReadLine());
+            int tinnerQuantityNeeded = int.Parse(Console.ReadLine());
+            int hoursPainterNeeded = int.Parse(Console.ReadLine());
+
+            double nylonCost = (nylonQuantityNeeded + 2) * 1.5;
+            double paintCost = (paintQuantityNeeded + (paintQuantityNeeded * 10/100)) * 14.50;
+            double tinnercost = tinnerQuantityNeeded * 5.00;
+
+         
+            double bags = 0.40;
+
+            double materialsCost = nylonCost + paintCost + tinnercost + bags;
+
+
+
+            double paintersLabour = (materialsCost * 30 / 100) * 8;
+
+            double totalCost = materialsCost + paintersLabour;
+
+            Console.WriteLine(totalCost);
+
+        }
+
+    }
+}
---
+++ First Steps in Coding/Yprajnenie 1/E1-7.cs
@@ -0,0 +1,27 @@
+﻿//using System;
+
+//namespace MyApp // Note: actual namespace depends on the project name.
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int chickenMenu = int.Parse(Console.ReadLine());
+//            int fishMenu = int.Parse(Console.ReadLine());
+//            int veganMenu = int.Parse(Console.ReadLine());
+
+//            double chickenMenuTotal = chickenMenu * 10.35;
+//            double fishMenuTotal = fishMenu * 12.40;
+//            double veganMenuTotal = veganMenu * 8.15;
+
+//            double totalMenu = chickenMenuTotal + fishMenuTotal + veganMenuTotal;
+
+//            double dessert = totalMenu * 20 / 100;
+//            double delivery = 2.5;
+//            double menuTotalCost = totalMenu + dessert + delivery;
+
+//            Console.WriteLine(menuTotalCost);
+//        }
+
+//    }
+//}
