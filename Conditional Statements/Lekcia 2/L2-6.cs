eacbc4c5286226d4f2fbc8cdf6ef4b21c5860727
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-05 23:12:39 +0000

    Update L2-6.cs

--- Conditional Statements/Lekcia 2/L2-6.cs
+++ Conditional Statements/Lekcia 2/L2-6.cs
@@ -1,18 +1,35 @@
-﻿//using System;
+using System;
 
-//namespace ConditionalStatements
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            double number = double.Parse(Console.ReadLine());
+namespace ConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
-//            if(number >= 5.50)
-//            {
-//                Console.WriteLine("Excellent!");
-//            }
-//        }
-//    }
-//}
+            double speed = double.Parse(Console.ReadLine());
+
+            if (speed <= 10)
+            {
+                Console.WriteLine("slow");
+            }
+            else if (speed > 10 && speed <= 50)
+            {
+                Console.WriteLine("average");
+            }
+            else if(speed > 50 && speed <= 150)
+            {
+                Console.WriteLine("fast");
+            }
+            else if(speed > 150 && speed <= 1000)
+            {
+                Console.WriteLine("ultra fast");
+            }
+            else
+            {
+                Console.WriteLine("extremely fast");
+            }
+        }
+    }
+}
 
