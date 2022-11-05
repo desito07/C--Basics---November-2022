e4226e55be6197af5ba4a20e554f709aa0b02a83
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-05 22:19:16 +0000

    Add files via upload

---
+++ Conditional Statements/Lekcia 2/L2-5.cs
@@ -0,0 +1,18 @@
+﻿//using System;
+
+//namespace ConditionalStatements
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            double number = double.Parse(Console.ReadLine());
+
+//            if(number >= 5.50)
+//            {
+//                Console.WriteLine("Excellent!");
+//            }
+//        }
+//    }
+//}
+
