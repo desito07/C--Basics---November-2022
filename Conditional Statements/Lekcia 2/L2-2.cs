20771ca6056abeff28b7dbca9b92e20b29cce604
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-05 22:20:12 +0000

    Update L2-2.cs

--- Conditional Statements/Lekcia 2/L2-2.cs
+++ Conditional Statements/Lekcia 2/L2-2.cs
@@ -1,23 +1,23 @@
-﻿//using System;
+using System;
 
-//namespace ConditionalStatements
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            int num1 = int.Parse(Console.ReadLine());
-//            int num2 = int.Parse(Console.ReadLine());
+namespace ConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
 
-//            if(num1 > num2)
-//            {
-//                Console.WriteLine(num1);
-//            }
-//            else
-//            {
-//                Console.WriteLine(num2);
-//            }
-//        }
-//    }
-//}
+            if (num1 > num2)
+            {
+                Console.WriteLine(num1);
+            }
+            else
+            {
+                Console.WriteLine(num2);
+            }
+        }
+    }
+}
 
