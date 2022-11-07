72e27ea50d690e50da028fe7038307d4b0a4089d
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-07 21:59:08 +0000

    Add files via upload

---
+++ Conditional Statements/Yprajnenie 2/E2-3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int min = int.Parse(Console.ReadLine());
+
+            min += 15;
+
+            if(min >= 60)
+            {
+                hour++;
+                min -= 60;
+            }
+
+            if (hour == 24)
+            {
+                hour = 0;
+            }
+
+            if ( min > 10)
+            {
+                Console.WriteLine($"{hour}:{min}");
+            }
+            else
+            {
+                Console.WriteLine($"{hour}:0{min}");
+            }
+        }
+    }
+
+}
