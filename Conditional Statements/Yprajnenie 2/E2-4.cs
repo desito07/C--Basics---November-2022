91e0c7d94ef5beeae0d5940dce54b8b2217ffb8c
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-09 00:21:41 +0000

    Add files via upload

--- Conditional Statements/Yprajnenie 2/E2-4.cs
+++ Conditional Statements/Yprajnenie 2/E2-4.cs
@@ -1,47 +1,45 @@
-﻿using System;
+﻿//using System;
 
-namespace ConditionalStatements
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            double tripCosts = double.Parse(Console.ReadLine());
-            int puzzels = int.Parse(Console.ReadLine());
-            int dolls = int.Parse(Console.ReadLine());
-            int bears = int.Parse(Console.ReadLine());
-            int minions = int.Parse(Console.ReadLine());
-            int trucks = int.Parse(Console.ReadLine());
+//namespace ConditionalStatements
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            double tripCosts = double.Parse(Console.ReadLine());
+//            int puzzels = int.Parse(Console.ReadLine());
+//            int dolls = int.Parse(Console.ReadLine());
+//            int bears = int.Parse(Console.ReadLine());
+//            int minions = int.Parse(Console.ReadLine());
+//            int trucks = int.Parse(Console.ReadLine());
 
-            double puzzelsPrice = puzzels * 2.60;
-            double dollsPrice = dolls * 3;
-            double bearsPrice = bears * 4.10;
-            double minionsPrice = minions * 8.20;
-            double trucksPrice = trucks * 2;
+//            double puzzelsPrice = puzzels * 2.60;
+//            double dollsPrice = dolls * 3;
+//            double bearsPrice = bears * 4.10;
+//            double minionsPrice = minions * 8.20;
+//            double trucksPrice = trucks * 2;
 
-            int toys = puzzels + dolls + bears + minions + trucks;
-            double totalPrice = puzzelsPrice + dollsPrice + bearsPrice + minionsPrice + trucksPrice;
+//            int toys = puzzels + dolls + bears + minions + trucks;
+//            double totalPrice = puzzelsPrice + dollsPrice + bearsPrice + minionsPrice + trucksPrice;
 
-            Console.WriteLine(totalPrice);
+//            if (toys >= 50)
+//            {
+//                totalPrice -= (totalPrice * 25 / 100.00);
+//            }
 
-            if (toys >= 50)
-            {
-                totalPrice -= (totalPrice * 25 / 100.00);
-            }
-
-            double rent = totalPrice * 0.1;
-            double total = totalPrice - rent;
+//            double rent = totalPrice * 0.1;
+//            double total = totalPrice - rent;
            
 
-            if (total > tripCosts)
-            {
-                Console.WriteLine($"Yes! {(total - tripCosts):F2} lv left.");
-            }
-            else
-            {
-                Console.WriteLine($"Not enough money! {(tripCosts - total):F2} lv needed.");
-            }
-        }
-    }
+//            if (total >= tripCosts)
+//            {
+//                Console.WriteLine($"Yes! {(total - tripCosts):F2} lv left.");
+//            }
+//            else
+//            {
+//                Console.WriteLine($"Not enough money! {(tripCosts - total):F2} lv needed.");
+//            }
+//        }
+//    }
 
-}
+//}
