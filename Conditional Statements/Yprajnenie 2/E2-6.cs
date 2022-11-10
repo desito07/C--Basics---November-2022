c1decc03836a91cbbb2eb7432bc07b0279bd3181
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-10 15:24:27 +0000

    Add files via upload

---
+++ Conditional Statements/Yprajnenie 2/E2-6.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordTime = double.Parse(Console.ReadLine());
+            double distanceMeters = double.Parse(Console.ReadLine());
+            double timeSecsPerMeter = double.Parse(Console.ReadLine());
+
+            double swimmerTime = distanceMeters * timeSecsPerMeter;
+            double delay = Math.Round(distanceMeters / 15 * 12.5);
+
+            double swimmerTimeTotal = swimmerTime + delay;
+
+            if(swimmerTime <= recordTime)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {swimmerTimeTotal:F2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No, he failed! He was {(swimmerTimeTotal - recordTime):F2} seconds slower.");
+            }
+
+        }
+    }
+
+}
