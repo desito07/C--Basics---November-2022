85fe96c5c94a0d44f2d552955b418c38d9439427
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-10 16:53:50 +0000

    Add files via upload

---
+++ Conditional Statements/Yprajnenie 2/E2-7.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budgetRequired = double.Parse(Console.ReadLine());
+            int videocards = int.Parse(Console.ReadLine());
+            int processors = int.Parse(Console.ReadLine());
+            int rammemory = int.Parse(Console.ReadLine());
+
+            double videocardCost = videocards * 250;
+            double processorsCost = videocardCost * 0.35 * processors;
+            double rammemoryCosts = videocardCost * 0.1 * rammemory;
+
+            double costs = videocardCost + processorsCost + rammemoryCosts;
+
+
+
+            if (videocards > processors)
+            {
+                costs -= costs * 0.15;
+            }
+
+            if (budgetRequired > costs)
+            {
+                Console.WriteLine($"You have {budgetRequired - costs:f2} leva left!");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {costs - budgetRequired:f2} leva more!");
+            }
+        }
+    }
+}
