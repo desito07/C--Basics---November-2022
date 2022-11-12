c04c441c0b5db6b921540456ec3443f845e6f2f3
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-12 22:36:19 +0000

    Update L3-8.cs

--- Conditional Statements Advanced/Lekcia 3/L3-8.cs
+++ Conditional Statements Advanced/Lekcia 3/L3-8.cs
@@ -1,13 +1,28 @@
-﻿//using System;
+using System;
 
-//namespace Conditional_Statements_Advanced
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-            
-//        }
-//    }
-//}
+namespace Conditional_Statements_Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string day = Console.ReadLine();
+            if (day == "Monday" || day == "Tuesday" || day ==  "Friday")
+            {
+                Console.WriteLine("12");
+            }
+            else if(day == "Wednesday" || day == "Thursday")
+            {
+                Console.WriteLine("14");
+            }
+            else
+            {
+                Console.WriteLine("16");
+            }
+            {
+
+            }
+        }
+    }
+}
 
