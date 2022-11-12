c4692013d5a139cc4d3d3bdf04c0f4e078ce6319
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-12 23:06:48 +0000

    Update L3-9.cs

--- Conditional Statements Advanced/Lekcia 3/L3-9.cs
+++ Conditional Statements Advanced/Lekcia 3/L3-9.cs
@@ -1,13 +1,29 @@
-﻿//using System;
+using System;
 
-//namespace Conditional_Statements_Advanced
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
+namespace Conditional_Statements_Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+
+
+            if (product == "banana" || product == "apple" || product == "kiwi" || product == "cherry" || product == "lemon" || product == "grapes")
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (product == "tomato" || product == "cucumber" || product == "pepper" || product == "carrot")
+            {
+                Console.WriteLine("vegetable");
+            }
+            else
+            {
+
+                Console.WriteLine("unknown");
+            }
             
-//        }
-//    }
-//}
+        }
+    }
+}
 
