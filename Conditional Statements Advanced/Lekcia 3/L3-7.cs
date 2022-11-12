4571feaf7f2d63d86b0bb50f5f657c3a63d04dcb
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-12 22:25:56 +0000

    Update L3-7.cs

--- Conditional Statements Advanced/Lekcia 3/L3-7.cs
+++ Conditional Statements Advanced/Lekcia 3/L3-7.cs
@@ -1,13 +1,22 @@
-﻿//using System;
+using System;
 
-//namespace Conditional_Statements_Advanced
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-            
-//        }
-//    }
-//}
+namespace Conditional_Statements_Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
 
+            if(hour >= 10 && hour <= 18 && day != "Sunday")
+            {
+                Console.WriteLine("open");
+            }
+            else
+            {
+                Console.WriteLine("closed");
+            }
+        }
+    }
+}
