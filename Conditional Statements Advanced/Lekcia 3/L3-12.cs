8e0dadde9f96b8b4a89aa58822e3b058edc0f126
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-14 22:33:31 +0000

    Update L3-12.cs

--- Conditional Statements Advanced/Lekcia 3/L3-12.cs
+++ Conditional Statements Advanced/Lekcia 3/L3-12.cs
@@ -1,13 +1,86 @@
-﻿//using System;
+using System;
+using System.Runtime.Intrinsics.X86;
 
-//namespace Conditional_Statements_Advanced
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-            
-//        }
-//    }
-//}
+namespace Conditional_Statements_Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = Console.ReadLine();
+            double sales = double.Parse(Console.ReadLine());
+            double commission = 0; 
+
+            bool inValid = false;
+
+            if (city == "Sofia")
+            {
+                if(sales <= 500 && sales >= 0)
+                {
+                    commission = sales * 0.05;
+                }
+                else if(sales > 500 && sales <= 1000)
+                {
+                    commission = sales * 0.07;
+                }
+                else if (sales > 1000 && sales <= 10000)
+                {
+                    commission = sales * 0.08;
+                }
+                else if (sales > 10000)
+                {
+                    commission = sales * 0.12;
+                }
+            }
+            else if (city == "Varna")
+            {
+                if (sales <= 500 && sales >= 0)
+                {
+                    commission = sales * 0.045;
+                }
+                else if (sales > 500 && sales <= 1000)
+                {
+                    commission = sales * 0.075;
+                }
+                else if (sales > 1000 && sales <= 10000)
+                {
+                    commission = sales * 0.1;
+                }
+                else if (sales > 10000)
+                {
+                    commission = sales * 0.13;
+                }
+            }
+            else if (city == "Plovdiv")
+            {
+                if (sales <= 500 && sales >= 0)
+                {
+                    commission = sales * 0.055;
+                }
+                else if (sales > 500 && sales <= 1000)
+                {
+                    commission = sales * 0.08;
+                }
+                else if (sales > 1000 && sales <= 10000)
+                {
+                    commission = sales * 0.12;
+                }
+                else if (sales > 10000)
+                {
+                    commission = sales * 0.145;
+                }
+            }
+
+            if((city == "Sofia" || city == "Varna" || city == "Plovdiv") && (commission > 0))
+            {
+                Console.WriteLine($"{commission:f2}");
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+                
+        }
+    }
+}
 
