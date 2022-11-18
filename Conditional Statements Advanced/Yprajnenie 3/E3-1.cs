bd95f9049469c823f60de6b9cb2dd6a935261b2e
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-18 17:44:02 +0000

    Update E3-1.cs

--- Conditional Statements Advanced/Yprajnenie 3/E3-1.cs
+++ Conditional Statements Advanced/Yprajnenie 3/E3-1.cs
@@ -1,13 +1,31 @@
-﻿//using System;
+using System;
 
-//namespace Conditional_Statements_Advanced
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-            
-//        }
-//    }
-//}
+namespace Conditional_Statements_Advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string screening = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int columns = int.Parse(Console.ReadLine());
+
+            double income = 0.0;
+
+            if(screening == "Premiere")
+            {
+                income = rows * columns * 12.00;
+            }
+            else if (screening == "Normal")
+            {
+                income = rows * columns * 7.5;
+            }
+            else if (screening == "Discount")
+            {
+                income = rows * columns * 5.00;
+            }
+            Console.WriteLine($"{income:f2} leva");
+        }
+    }
+}
 
