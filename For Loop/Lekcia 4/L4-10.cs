5491fdf4b3de4ca36b59b4f83c3ead731aa6ed58
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-21 23:07:04 +0000

    Update L4-10.cs

--- For Loop/Lekcia 4/L4-10.cs
+++ For Loop/Lekcia 4/L4-10.cs
@@ -1,9 +1,41 @@
-﻿namespace For_Loop;
-class Program
+using System;
+
+namespace L4_10
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int leftSum = 0;
+            int rightSum = 0;
+
+            for (int i = 1; i <= num; i++)
+            {
+
+                int currentNum = int.Parse(Console.ReadLine());
+
+                if (i % 2 == 0)
+                {
+                    leftSum += currentNum;
+                }
+                else
+                {
+                    rightSum += currentNum;
+                }
+            }
+            if (leftSum == rightSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {leftSum}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(rightSum - leftSum)}");
+            }
+        }
     }
 }
+
 
