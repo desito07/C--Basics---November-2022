aecc39019715cbb75ae4f7f165cb0a414e745aac
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-21 14:42:35 +0000

    Update L4-8.cs

--- For Loop/Lekcia 4/L4-8.cs
+++ For Loop/Lekcia 4/L4-8.cs
@@ -1,9 +1,31 @@
-﻿namespace For_Loop;
-class Program
+
+using System;
+
+namespace For_Loop
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            int numbersN = int.Parse(Console.ReadLine());
+            int smallest = int.MaxValue;
+            int biggest = int.MinValue;
+
+            for (int i = 0; i < numbersN; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                if(currentNumber < smallest)
+                {
+                    smallest = currentNumber;
+                }
+                if(currentNumber >= biggest)
+                {
+                    biggest = currentNumber;
+                }
+            }
+            Console.WriteLine($"Max number: {biggest}");
+            Console.WriteLine($"Min number: {smallest}");
+
+        }
     }
 }
-
