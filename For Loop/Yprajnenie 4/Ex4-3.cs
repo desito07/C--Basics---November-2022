ccf2394beb64957dd29f96dbb957c8816501707c
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-22 18:29:32 +0000

    Update Ex4-3.cs

--- For Loop/Yprajnenie 4/Ex4-3.cs
+++ For Loop/Yprajnenie 4/Ex4-3.cs
@@ -1,13 +1,49 @@
-﻿//using System;
+using System;
 
-//namespace For_Loop_Exc
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//        }
-//    }
-//}
+namespace For_Loop_Exc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int p1 = 0;
+            int p2 = 0;
+            int p3 = 0;
+            int p4 = 0;
+            int p5 = 0;
 
+            for (int i = 0; i < num; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                if (currentNum < 200)
+                {
+                    p1++;
+                }
+                else if (currentNum >= 200 && currentNum < 400)
+                {
+                    p2++;
+                }
+                else if (currentNum >= 400 && currentNum < 600)
+                {
+                    p3++;
+                }
+                else if (currentNum >= 600 && currentNum < 800)
+                {
+                    p4++;
+
+                }
+                else if (currentNum >= 800)
+                {
+                    p5++;
+                }
+            }
+            Console.WriteLine($"{p1 * 1.0 / num * 100:f2}%");
+            Console.WriteLine($"{p2 * 1.0 / num * 100:f2}%");
+            Console.WriteLine($"{p3 * 1.0 / num * 100:f2}%");
+            Console.WriteLine($"{p4 * 1.0 / num * 100:f2}%");
+            Console.WriteLine($"{p5 * 1.0 / num * 100:f2}%");
+        }
+    }
+    
+}
