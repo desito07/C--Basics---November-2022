9f40ca1b969eb3f831a67c9c99685e1573ff7c9d
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-22 16:47:43 +0000

    Add files via upload

---
+++ For Loop/Yprajnenie 4/Ex4-2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace For_Loop_Exc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int maxNumber = int.MinValue;
+
+            for (int i = 0; i < num; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                sum += currentNum;
+                if( currentNum > maxNumber)
+                {
+                    maxNumber = currentNum;
+                }
+
+
+            }
+            int sumWithoutMaxNumber = sum - maxNumber;
+            if(sumWithoutMaxNumber == maxNumber)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {sumWithoutMaxNumber}");
+
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(sumWithoutMaxNumber - maxNumber)}");
+            }
+        }
+    }
+}
+
+  
