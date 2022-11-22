d3dcb136f39e1d35d63f3e92711faaf80e1a6f4a
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-22 22:35:48 +0000

    Update Ex4-5.cs

--- For Loop/Yprajnenie 4/Ex4-5.cs
+++ For Loop/Yprajnenie 4/Ex4-5.cs
@@ -1,13 +1,42 @@
-﻿//using System;
+using System;
+using System.Collections.Generic;
 
-//namespace For_Loop_Exc
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//        }
-//    }
-//}
+namespace For_Loop_Exc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tabsNum = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+           
+            
 
+            for (int i = 0; i < tabsNum; i++)
+            {
+                string website = Console.ReadLine();
+                if(website == "Facebook")
+                {
+                    salary -= 150;
+                }
+                else if(website == "Instagram")
+                {
+                    salary -= 100;
+                }
+                else if(website == "Reddit")
+                {
+                    salary -= 50;
+                }
+            }
+            if(salary <= 0)
+            {
+                Console.WriteLine("You have lost your salary.");
+            }
+            else
+            {
+                Console.WriteLine(salary);
+            }
+            
+        }
+    }
+}
