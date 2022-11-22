d9a98100fa85126b45b812130580f35281afd3cc
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-22 09:20:29 +0000

    Update E4-1.cs

--- For Loop/Yprajnenie 4/E4-1.cs
+++ For Loop/Yprajnenie 4/E4-1.cs
@@ -1,9 +1,20 @@
-﻿namespace For_Loop;
-class Program
+using System;
+
+namespace _1234
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            for (int i = 7; i <= 1000; i++)
+            {
+                if(i % 10 == 7)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+    
+        }
     }
 }
 
