379641c656cd4065e3f0a31e0f03ea0bdbd0c02e
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-23 13:07:42 +0000

    Update Ex4-6.cs

--- For Loop/Yprajnenie 4/Ex4-6.cs
+++ For Loop/Yprajnenie 4/Ex4-6.cs
@@ -1,13 +1,40 @@
-﻿//using System;
+using System;
 
-//namespace For_Loop_Exc
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//        }
-//    }
-//}
+namespace For_Loop_Exc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameActor = Console.ReadLine();
+            double startingPoints = double.Parse(Console.ReadLine());
+            int reviewersCount = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < reviewersCount; i++)
+            {
+                string reviewerName = Console.ReadLine();
+
+                double reviewerPoints = double.Parse(Console.ReadLine());
+
+                double newPoints = reviewerName.Length * reviewerPoints / 2;
+
+                startingPoints += newPoints;
+
+                if(startingPoints > 1250.5)
+                {
+                    break;
+                }
+
+            }
+            if( startingPoints > 1250.5)
+            {
+                Console.WriteLine($"Congratulations, {nameActor} got a nominee for leading role with {startingPoints:f1}!");
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, {nameActor} you need {(1250.5 - startingPoints):f1} more!");
+            }
+        }
+    }
+}
 
