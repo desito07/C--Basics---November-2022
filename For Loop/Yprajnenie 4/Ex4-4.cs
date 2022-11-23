69fbb06a3f1389e82c4007b6f3216abc75f7fa36
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-23 19:28:45 +0000

    Update Ex4-4.cs

--- For Loop/Yprajnenie 4/Ex4-4.cs
+++ For Loop/Yprajnenie 4/Ex4-4.cs
@@ -1,13 +1,40 @@
-﻿//using System;
+using System;
 
-//namespace For_Loop_Exc
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//        }
-//    }
-//}
+namespace For_Loop_Exc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            double washingMachinePrice = double.Parse(Console.ReadLine());
+            double toyPriceSingle = double.Parse(Console.ReadLine());
+            double savings = 0;
+            double money = 10;
+
+            for (int i = 1; i <= age; i++)
+            {
+                if (i % 2 == 0)
+                {
+                   
+                    savings += money;
+                    money += 10;
+                    savings--;
+                }
+                else
+                {
+                    savings += toyPriceSingle;
+                }
+            }
+            if (savings >= washingMachinePrice)
+            {
+                Console.WriteLine($"Yes! {savings - washingMachinePrice:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {washingMachinePrice - savings:f2}");
+            }
+        }
+    }
+}
 
