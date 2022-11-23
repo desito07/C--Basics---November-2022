14cb7226c8e11d401e1944dd6c470a13fa4e2fae
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-23 21:54:32 +0000

    Add files via upload

---
+++ Exams Prep/Online Exam - 28 and 29 March 2020/1_Birthday Party.cs
@@ -0,0 +1,19 @@
+﻿//using System;
+
+//namespace Exams_Prep
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            double rent = double.Parse(Console.ReadLine());
+
+//            double cake = rent * 0.20;
+//            double drinks = cake - cake * 0.45;
+//            double animator = rent * 1 / 3;
+
+//            Console.WriteLine(rent + cake + drinks + animator) ;
+//        }
+//    }
+//}
+
---
+++ Exams Prep/Online Exam - 28 and 29 March 2020/2_Change Bureau.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exams_Prep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bitcoin = int.Parse(Console.ReadLine());
+            double uan = double.Parse(Console.ReadLine());
+            double commisson = double.Parse(Console.ReadLine());
+
+            double usd = 1.76;
+            double uanInUSD = uan * 0.15;
+
+            double leva = bitcoin * 1168 + uanInUSD * 1.76;
+            double euro = leva / 1.95;
+            
+            double total = euro * commisson / 100;
+           
+            Console.WriteLine($"{euro - total:f2}");
+        }
+    }
+}
+
