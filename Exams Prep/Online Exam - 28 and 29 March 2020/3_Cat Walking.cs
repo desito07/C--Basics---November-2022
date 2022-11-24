8e3c31941c6a38c64504ea62369564ba54ad3fc8
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-24 16:54:57 +0000

    Update 3_Cat Walking.cs

--- Exams Prep/Online Exam - 28 and 29 March 2020/3_Cat Walking.cs
+++ Exams Prep/Online Exam - 28 and 29 March 2020/3_Cat Walking.cs
@@ -1,13 +1,28 @@
-﻿//using System;
+using System;
 
-//namespace Exams_Prep
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//        }
-//    }
-//}
+namespace Exams_Prep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutesPerDay = int.Parse(Console.ReadLine());
+            int numWalksPerday = int.Parse(Console.ReadLine());
+            int caloriesPerDay = int.Parse(Console.ReadLine());
+
+            double minTotal = minutesPerDay * numWalksPerday;
+            double caloriesTotal = minTotal * 5;
+            double calories50 = caloriesPerDay * 0.5;
+
+            if(caloriesTotal >= calories50)
+            {
+                Console.WriteLine($"Yes, the walk for your cat is enough. Burned calories per day: {caloriesTotal}.");
+            }
+            else
+            {
+                Console.WriteLine($"No, the walk for your cat is not enough. Burned calories per day: {caloriesTotal}.");
+            }
+        }
+    }
+}
 
