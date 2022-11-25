2bc193b91a02256ddc8dc87213af60f79097a7fb
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-25 05:52:41 +0000

    Update 4_Mountain Run.cs

--- Exams Prep/Online Exam - 28 and 29 March 2020/4_Mountain Run.cs
+++ Exams Prep/Online Exam - 28 and 29 March 2020/4_Mountain Run.cs
@@ -1,13 +1,31 @@
-﻿//using System;
+using System;
 
-//namespace Exams_Prep
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//        }
-//    }
-//}
+
+namespace Exams_Prep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordSecs = double.Parse(Console.ReadLine());
+            double distanceMeters = double.Parse(Console.ReadLine());
+            double timeSecsPerMeter = double.Parse(Console.ReadLine());
+
+            double secsTotal = distanceMeters * timeSecsPerMeter;
+            double delay = (Math.Floor(distanceMeters / 50)) * 30;
+            double totalTime = (secsTotal + delay);
+
+
+
+            if (recordSecs > totalTime)
+            {
+                Console.WriteLine($"Yes! The new record is {totalTime:f2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No! He was {totalTime - recordSecs:f2} seconds slower.");
+            }
+        }
+    }
+}
 
