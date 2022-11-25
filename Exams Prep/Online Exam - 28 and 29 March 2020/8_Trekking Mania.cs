832a2218aeb4954cd1a15b5e504c2e1c4f798194
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-25 11:55:00 +0000

    Update 8_Trekking Mania.cs

--- Exams Prep/Online Exam - 28 and 29 March 2020/8_Trekking Mania.cs
+++ Exams Prep/Online Exam - 28 and 29 March 2020/8_Trekking Mania.cs
@@ -1,13 +1,55 @@
-﻿//using System;
+using System;
 
-//namespace Exams_Prep
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//        }
-//    }
-//}
+namespace For_Loop_Exc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int groupNum = int.Parse(Console.ReadLine());
+            double groupMusala = 0.00;
+            double groupMonblan = 0.00;
+            double groupKilimandjaro = 0.00;
+            double groupK2 = 0.00;
+            double groupEverest = 0.00;
+            double sum = 0;
 
+            for (int i = 0; i < groupNum; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                if (currentNum <= 5)
+                {
+                    groupMusala += currentNum;
+                    sum += currentNum;
+                }
+                else if (currentNum >= 6 && currentNum <= 12)
+                {
+                    groupMonblan += currentNum;
+                    sum += currentNum;
+                }
+                else if (currentNum >= 13 && currentNum <= 25)
+                {
+                    groupKilimandjaro += currentNum;
+                    sum += currentNum;
+                }
+                else if (currentNum >= 26 && currentNum <= 40)
+                {
+                    groupK2 += currentNum;
+                    sum += currentNum;
+
+                }
+                else if (currentNum >= 41)
+                {
+                    groupEverest += currentNum;
+                    sum += currentNum;
+                }
+            }
+            Console.WriteLine($"{groupMusala / sum * 100:f2}%");
+            Console.WriteLine($"{groupMonblan / sum * 100:f2}%");
+            Console.WriteLine($"{groupKilimandjaro / sum * 100:f2}%");
+            Console.WriteLine($"{groupK2 / sum * 100:f2}%");
+            Console.WriteLine($"{groupEverest / sum * 100:f2}%");
+        }
+    }
+
+}
