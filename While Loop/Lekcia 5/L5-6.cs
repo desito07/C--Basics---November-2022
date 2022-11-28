60c750cc36454d767fae56a6e092dd27a117b3e6
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-28 23:31:25 +0000

    Update L5-6.cs

--- While Loop/Lekcia 5/L5-6.cs
+++ While Loop/Lekcia 5/L5-6.cs
@@ -1,27 +1,25 @@
-﻿//using System;
+using System;
 
-//namespace For_Loop
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            string name = Console.ReadLine();
-//            int sum = 0;
+namespace For_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            double maxNum = double.MinValue;
 
-//            for (int i = 0; i < name.Length; i++)
-//            {
-//                switch (name[i])
-//                {
-//                    case 'a': sum += 1; break;
-//                    case 'e': sum += 2; break;
-//                    case 'i': sum += 3; break;
-//                    case 'o': sum += 4; break;
-//                    case 'u': sum += 5; break;
-//                }
-//            }
-//            Console.WriteLine(sum);
+            while(command != "Stop")
+            {
+                double commandNum = double.Parse(command);
+                if (commandNum > maxNum)
+                {
+                    maxNum = commandNum;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(maxNum);
 
-//        }
-//    }
-//}
+        }
+    }
+}
