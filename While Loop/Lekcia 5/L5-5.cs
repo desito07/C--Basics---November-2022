0bd9a7c79f45c5d922e382102522f6b854f7def9
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2022-11-28 23:09:00 +0000

    Update L5-5.cs

--- While Loop/Lekcia 5/L5-5.cs
+++ While Loop/Lekcia 5/L5-5.cs
@@ -1,16 +1,33 @@
-﻿//using System;
+using System;
 
-//namespace For_Loop
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            string name = Console.ReadLine();
-//            for(int i = 0; i < name.Length; i++)
-//            {
-//                Console.WriteLine(name[i]);
-//            }
-//        }
-//    }
-//}
+namespace For_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            double sum = 0.0;
+
+            while(command != "NoMoreMoney")
+            {
+
+                double commandNum = double.Parse(command);
+
+                if (commandNum < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                Console.WriteLine($"Increase: {commandNum:f2}");
+
+                sum += commandNum;
+
+                command = Console.ReadLine();
+
+                
+            }
+            Console.WriteLine($"Total: {sum:f2}");
+        }
+    }
+}
