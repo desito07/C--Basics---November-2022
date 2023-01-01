4536d3683b86b0964d6c29fb46cb3111dc3aab46
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-01 15:17:25 +0000

    Update E5-6.cs

--- While Loop/Yprajnenie 5/E5-6.cs
+++ While Loop/Yprajnenie 5/E5-6.cs
@@ -1,5 +1,44 @@
-﻿using System;
+using System;
 
 namespace L4_10
 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            int count = width * height;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "STOP")
+                {
+                    Console.WriteLine($"{count} pieces are left.");
+                    break;
+                }
+
+                int piecesToTake = int.Parse(command);
+
+                if (piecesToTake > count)
+                {
+                    Console.WriteLine($"No more cake left! You need {piecesToTake - count} pieces more.");
+                    break;
+                }
+
+                count -= piecesToTake;
+
+                if (count == 0)
+                {
+                    Console.WriteLine($"{count} pieces are left.");
+                    break;
+                }
+            }
+        }
+
+    }
+}
+
 
