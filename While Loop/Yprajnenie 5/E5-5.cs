dfe4778e920dc448868a11950782d5817c8f0c47
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-01 14:25:21 +0000

    Update E5-5.cs

--- While Loop/Yprajnenie 5/E5-5.cs
+++ While Loop/Yprajnenie 5/E5-5.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace L4_10
 {
@@ -6,36 +6,56 @@ namespace L4_10
     {
         static void Main(string[] args)
         {
-            int num = int.Parse(Console.ReadLine());
-            int leftSum = 0;
-            int rightSum = 0;
+            decimal change = decimal.Parse(Console.ReadLine());
+            int counter = 0;
 
-            for (int i = 1; i <= num; i++)
+            while (change > 0)
             {
+            
+                    if (change >= 2)
+                    {
+                        change -= 2;
+                        counter++;
+                    }
+                    else if (change >= 1)
+                    {
+                        change -= 1;
+                        counter++;
+                    }
+                    else if (change >= 0.5m)
+                    {
+                        change -= 0.5m;
+                        counter++;
+                    }
+                    else if (change >= 0.2m)
+                    {
+                        change -= 0.2m;
+                        counter++;
+                    }
+                    else if (change >= 0.1m)
+                    {
+                        change -= 0.1m;
+                        counter++;
+                    }
+                    else if (change >= 0.05m)
+                    {
+                        change -= 0.05m;
+                        counter++;
+                    }
+                else if (change >= 0.02m)
+                    {
+                        change -= 0.02m;
+                        counter++;
+                    }
+                    else
+                    {
+                        change -= 0.01m;
+                        counter++;
+                    }
+            }
+            Console.WriteLine(counter);
+           
 
-                int currentNum = int.Parse(Console.ReadLine());
-
-                if (i % 2 == 0)
-                {
-                    leftSum += currentNum;
-                }
-                else
-                {
-                    rightSum += currentNum;
-                }
-            }
-            if (leftSum == rightSum)
-            {
-                Console.WriteLine("Yes");
-                Console.WriteLine($"Sum = {leftSum}");
-            }
-            else
-            {
-                Console.WriteLine("No");
-                Console.WriteLine($"Diff = {Math.Abs(rightSum - leftSum)}");
-            }
         }
     }
 }
-
-
