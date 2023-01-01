9a7727545321f4dd83b3b5b49617238f05570878
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-01 14:26:37 +0000

    Update E5-6.cs

--- While Loop/Yprajnenie 5/E5-6.cs
+++ While Loop/Yprajnenie 5/E5-6.cs
@@ -2,40 +2,4 @@
 
 namespace L4_10
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int num = int.Parse(Console.ReadLine());
-            int leftSum = 0;
-            int rightSum = 0;
-
-            for (int i = 1; i <= num; i++)
-            {
-
-                int currentNum = int.Parse(Console.ReadLine());
-
-                if (i % 2 == 0)
-                {
-                    leftSum += currentNum;
-                }
-                else
-                {
-                    rightSum += currentNum;
-                }
-            }
-            if (leftSum == rightSum)
-            {
-                Console.WriteLine("Yes");
-                Console.WriteLine($"Sum = {leftSum}");
-            }
-            else
-            {
-                Console.WriteLine("No");
-                Console.WriteLine($"Diff = {Math.Abs(rightSum - leftSum)}");
-            }
-        }
-    }
-}
-
 
