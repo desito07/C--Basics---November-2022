842959184cf2d2c74276ab6d532f854600fea574
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-03 23:20:38 +0000

    Update L6-4.cs

--- Nested Loops/Lekcia 6/L6-4.cs
+++ Nested Loops/Lekcia 6/L6-4.cs
@@ -1,13 +1,40 @@
-﻿//using System;
+using System;
 
-//namespace Nested_Loops
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//        }
-//    }
-//}
+namespace Nested_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int final = int.Parse(Console.ReadLine());
+            int magic = int.Parse(Console.ReadLine());
+            int combinations = 0;
+            bool isFound = false;
 
+            for (int i = start; i <= final; i++)
+            {
+                for (int j = start; j <= final; j++)
+                {
+                    combinations++;
+                    int sum = i + j;
+                    if (sum == magic)
+                    {
+                        isFound = true;
+                        Console.WriteLine($"Combination N:{combinations} ({i} + {j} = {sum})");
+                        break;
+                    }
+                }
+                if (isFound == true)
+                {
+                    break;
+                } 
+            }
+            if (isFound == false)
+            {
+                Console.WriteLine($"{combinations} combinations - neither equals {magic}");
+                
+            }
+        }
+    }
+}
