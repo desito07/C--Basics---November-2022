627052a9e39818c0f27cf394263dd00db1897763
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-03 21:12:21 +0000

    Update L6-3.cs

--- Nested Loops/Lekcia 6/L6-3.cs
+++ Nested Loops/Lekcia 6/L6-3.cs
@@ -1,13 +1,30 @@
-﻿//using System;
+using System;
 
-//namespace Nested_Loops
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//        }
-//    }
-//}
+namespace Nested_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counter = 0;
+
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    for (int k = 0; k <= n; k++)
+                    {
+                        if (i + j + k == n)
+                        {
+                            counter++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+
+        }
+    }
+}
 
