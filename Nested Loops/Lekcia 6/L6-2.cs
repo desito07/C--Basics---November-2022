620e4e560c8f1d08c60ff1ad7ed98f89dca38af6
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-03 17:10:07 +0000

    Update L6-2.cs

--- Nested Loops/Lekcia 6/L6-2.cs
+++ Nested Loops/Lekcia 6/L6-2.cs
@@ -1,13 +1,20 @@
-﻿//using System;
+using System;
 
-//namespace Nested_Loops
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//        }
-//    }
-//}
+namespace Nested_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine($"{i} * {j} = {i * j}");
+                }
+            }
+        }
+    }
+}
 
