c28bc195ed81360356678b58a7b694753f09dd46
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-03 17:00:58 +0000

    Add files via upload

---
+++ Nested Loops/Lekcia 6/L6-1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Nested_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int hour = 0; hour < 24; hour++)
+            {
+                for (int min = 0; min <= 59; min++)
+                {
+                    Console.WriteLine($"{hour}:{min}");
+                }
+            }
+        }
+    }
+}
+
