beeaddb1daa0fc5046ffded103d6eeb2100f112f
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-04 14:52:23 +0000

    Update L6-5.cs

--- Nested Loops/Lekcia 6/L6-5.cs
+++ Nested Loops/Lekcia 6/L6-5.cs
@@ -1,13 +1,30 @@
-﻿//using System;
+using System;
 
-//namespace Nested_Loops
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//        }
-//    }
-//}
+namespace Nested_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+
+            while (destination != "End")
+            {
+                double tripPrice = double.Parse(Console.ReadLine());
+                double sum = 0.0;
+                while (sum < tripPrice)
+                {
+                    double instalment = double.Parse(Console.ReadLine());
+                    sum += instalment;
+                }
+                if(sum >= tripPrice)
+                {
+                    Console.WriteLine($"Going to {destination}!");
+                }
+                destination = Console.ReadLine();
+            }
+            
+        }
+    }
+}
 
