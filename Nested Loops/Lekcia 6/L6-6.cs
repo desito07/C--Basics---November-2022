b8279b617b422d36254eec66e00ad811255dc8ff
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-04 15:50:53 +0000

    Update L6-6.cs

--- Nested Loops/Lekcia 6/L6-6.cs
+++ Nested Loops/Lekcia 6/L6-6.cs
@@ -1,13 +1,38 @@
-﻿//using System;
+using System;
 
-//namespace Nested_Loops
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//        }
-//    }
-//}
+namespace Nested_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int floors = int.Parse(Console.ReadLine());
+            int rooms = int.Parse(Console.ReadLine());
+            
+
+            for (int currentFloor = floors; currentFloor >= 1; currentFloor--)
+            {
+                string floorTemplate = "";
+                
+                for (int currentRoom = 0; currentRoom < rooms; currentRoom++)
+                {
+                    if(currentFloor == floors)
+                    {
+                        floorTemplate += $"L{currentFloor}{currentRoom} ";
+                    }
+                    else if(currentFloor % 2 == 0)
+                    {
+                        floorTemplate += $"O{currentFloor}{currentRoom} ";
+                    }
+                    else
+                    {
+                        floorTemplate += $"A{currentFloor}{currentRoom} ";
+                    }
+                    
+                }
+                Console.WriteLine(floorTemplate);
+            }
+        }
+    }
+}
 
