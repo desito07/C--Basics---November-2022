32024d13f516b7e817509bd8512efb813e260b3b
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-04 22:34:06 +0000

    Update E6-1.cs

--- Nested Loops/Yprajnenie 6/E6-1.cs
+++ Nested Loops/Yprajnenie 6/E6-1.cs
@@ -1,13 +1,34 @@
-﻿//using System;
+using System;
 
-//namespace Nested_Loops
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//        }
-//    }
-//}
+namespace Nested_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int numToPrintInRow = 1;
+            int currentNumber = 1;
+
+            while (currentNumber <= number)
+            {
+                for (int i = 0; i < numToPrintInRow; i++)
+                {
+                    Console.Write(currentNumber + " ");
+                    currentNumber++;
+
+                    if (currentNumber > number)
+                    {
+                        break;
+                    }
+
+                }
+
+                Console.WriteLine();
+                numToPrintInRow++;
+
+            }
+        }
+    }
+}
 
