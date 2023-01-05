23c5d41f3443dc35b01811f87f4c207966a34080
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-05 22:30:39 +0000

    Update E6-4.cs

--- Nested Loops/Yprajnenie 6/E6-4.cs
+++ Nested Loops/Yprajnenie 6/E6-4.cs
@@ -1,13 +1,39 @@
-﻿//using System;
+using System;
 
-//namespace Nested_Loops
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//        }
-//    }
-//}
+namespace Nested_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int judgesCount = int.Parse(Console.ReadLine());
+            double presentationsAvrGradesSum = 0;
+            int presentationsCount = 0;
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if(command == "Finish")
+                {
+                    break;
+                }
+
+                presentationsCount++;
+
+                double gradesSum = 0;
+
+                for (int i = 0; i < judgesCount; i++)
+                {
+                    gradesSum += double.Parse(Console.ReadLine()); 
+                }
+                presentationsAvrGradesSum += gradesSum / judgesCount;
+                Console.WriteLine($"{command} - {gradesSum / judgesCount:f2}.");
+            }
+
+
+            Console.WriteLine($"Student's final assessment is {presentationsAvrGradesSum / presentationsCount:f2}.");
+        }
+    }
+}
 
