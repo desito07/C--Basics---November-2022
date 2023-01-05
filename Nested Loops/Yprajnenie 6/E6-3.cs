0801079231aea7cd23b9e4f1da9d5741351f9213
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-05 21:44:34 +0000

    Update E6-3.cs

--- Nested Loops/Yprajnenie 6/E6-3.cs
+++ Nested Loops/Yprajnenie 6/E6-3.cs
@@ -1,13 +1,53 @@
-﻿//using System;
+using System;
 
-//namespace Nested_Loops
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//        }
-//    }
-//}
+namespace Nested_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primeNumbersSum = 0, nonPrimeNumbersSum = 0;
+            while (true)
+            {
+                string command = Console.ReadLine();
+                if(command == "stop")
+                {
+                    break;
+                }
+
+                int number = int.Parse(command);
+
+                if(number < 0)
+                {
+                    Console.WriteLine("Number is negative.");
+                    continue;
+                }
+
+                bool isPrime = true;
+
+                for (int i = 2; i < number; i++)
+                {
+                    if(number % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    primeNumbersSum += number;
+                }
+                else
+                {
+                    nonPrimeNumbersSum += number;
+                }
+
+            }
+
+            Console.WriteLine($"Sum of all prime numbers is: {primeNumbersSum}");
+            Console.WriteLine($"Sum of all non prime numbers is: {nonPrimeNumbersSum}");
+        }
+    }
+}
 
