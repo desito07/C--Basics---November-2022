93c052754f0c2f2d5c9edf2976b97122aa589b92
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-05 17:21:21 +0000

    Update E6-2.cs

--- Nested Loops/Yprajnenie 6/E6-2.cs
+++ Nested Loops/Yprajnenie 6/E6-2.cs
@@ -1,13 +1,39 @@
-﻿//using System;
+using System;
 
-//namespace Nested_Loops
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//        }
-//    }
-//}
+namespace Nested_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            for (int i = start; i <= end; i++)
+            {
+                int oddSum = 0, evenSum = 0;
+
+                string numberAsText = i.ToString();
+
+                for (int pos = 0; pos < numberAsText.Length; pos++)
+                {
+                    int numberAtCurrentPos = int.Parse(numberAsText[pos].ToString());
+                    if((pos + 1) % 2 == 0)
+                    {
+                        evenSum += numberAtCurrentPos;
+                    }
+                    else
+                    {
+                        oddSum += numberAtCurrentPos;
+                    }
+                }
+
+                if (oddSum == evenSum)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
 
