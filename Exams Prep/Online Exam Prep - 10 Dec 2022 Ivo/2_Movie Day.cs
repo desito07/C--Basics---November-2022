037eb61503c14f9b7f076b4e3d3218ae0bbc83ff
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-07 21:50:04 +0000

    Update 2_Movie Day.cs

--- Exams Prep/Online Exam Prep - 10 Dec 2022 Ivo/2_Movie Day.cs
+++ Exams Prep/Online Exam Prep - 10 Dec 2022 Ivo/2_Movie Day.cs
@@ -1,29 +1,29 @@
-﻿//using System;
+using System;
 
-//namespace Exams_Prep
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            int timeScreening = int.Parse(Console.ReadLine());
-//            int scenes = int.Parse(Console.ReadLine());
-//            int durationScene = int.Parse(Console.ReadLine());
+namespace Exams_Prep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int timeScreening = int.Parse(Console.ReadLine());
+            int scenes = int.Parse(Console.ReadLine());
+            int durationScene = int.Parse(Console.ReadLine());
 
-//            double prepTeren = timeScreening * 0.15;
-//            double totalTimescenes = scenes * durationScene;
+            double prepTeren = timeScreening * 0.15;
+            double totalTimescenes = scenes * durationScene;
 
-//            double totalTime = prepTeren + totalTimescenes;
+            double totalTime = prepTeren + totalTimescenes;
 
-//            if (timeScreening <= totalTime)
-//            {
-//                Console.WriteLine($"Time is up! To complete the movie you need {Math.Round(totalTime - timeScreening)} minutes.");
-//            }
-//            else
-//            {
-//                Console.WriteLine($"You managed to finish the movie on time! You have {Math.Round(timeScreening - totalTime)} minutes left!");
-//            }
-//        }
-//    }
-//}
+            if (timeScreening <= totalTime)
+            {
+                Console.WriteLine($"Time is up! To complete the movie you need {Math.Round(totalTime - timeScreening)} minutes.");
+            }
+            else
+            {
+                Console.WriteLine($"You managed to finish the movie on time! You have {Math.Round(timeScreening - totalTime)} minutes left!");
+            }
+        }
+    }
+}
 
