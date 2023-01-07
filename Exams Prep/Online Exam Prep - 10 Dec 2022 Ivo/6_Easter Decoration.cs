5fc3cb8d8b80e161b7c41a3c1a5c5148035d8f1b
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-07 21:50:57 +0000

    Update 6_Easter Decoration.cs

--- Exams Prep/Online Exam Prep - 10 Dec 2022 Ivo/6_Easter Decoration.cs
+++ Exams Prep/Online Exam Prep - 10 Dec 2022 Ivo/6_Easter Decoration.cs
@@ -1,13 +1,1 @@
-﻿//using System;
-
-//namespace Exams_Prep
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            Console.WriteLine("Hello World!");
-//        }
-//    }
-//}
 
