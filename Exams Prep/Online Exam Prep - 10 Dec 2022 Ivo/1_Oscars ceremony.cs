0bd863351a80ee8c90a3f99b3671d3ce0e4e1077
Desislava Andreeva <d_andreeva82@yahoo.co.uk>
2023-01-07 21:49:40 +0000

    Update 1_Oscars ceremony.cs

--- Exams Prep/Online Exam Prep - 10 Dec 2022 Ivo/1_Oscars ceremony.cs
+++ Exams Prep/Online Exam Prep - 10 Dec 2022 Ivo/1_Oscars ceremony.cs
@@ -1,21 +1,23 @@
-﻿//using System;
+using System;
 
-//namespace Exams_Prep
-//{
-//    class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            int rent = int.Parse(Console.ReadLine());
+namespace Exams_Prep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rent = int.Parse(Console.ReadLine());
 
-//            double statues = rent - rent * 0.30;
-//            double catering = statues - statues * 0.15;
-//            double sounding = catering * 0.5;
-//            double cost = (rent + statues + catering + sounding);
+            double statues = rent - rent * 0.30;
+            double catering = statues - statues * 0.15;
+            double sounding = catering * 0.5;
+            double cost = (rent + statues + catering + sounding);
 
-//            Console.WriteLine($"{cost:f2}");
+            Console.WriteLine($"{cost:f2}");
 
-//        }
-//    }
-//}
+        }
+    }
+}
+
+
 
